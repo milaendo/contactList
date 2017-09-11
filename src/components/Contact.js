6fd556d9cb134516880db8df195ee5eb4041c1ff
Mila Endo <milal.alim@gmail.com>
2017-09-11 14:09:28 -0700

    fixed picture display

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,7 +20,7 @@ class Contact extends Component {
 			<Link to='/ListView' ><img src='http://www.free-icons-download.net/images/green-back-button-icon-65921.png' alt='button' id='backbutton'/> </Link>
 			<h1>{this.props.name.first}'s Contact Information </h1>
 				<div className='contactWrap'>
-					<img src='https://image.freepik.com/free-icon/user-image-with-black-background_318-34564.jpg' alt='anon' className='anon'/>
+					<img src={this.props.picture ? this.props.picture.large : ""} alt='anon' className='anon'/>
 					<div>{this.props.name.first} {this.props.name.last}</div>
 					<div>{this.props.email}</div>
 					<div>{this.props.phone}</div>
